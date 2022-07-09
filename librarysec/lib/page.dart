2387df57a337d9258c2f66884bc9ab489d7912ea
Valdo <85319750+valdodotcom@users.noreply.github.com>
2022-07-09 15:29:14 +0000

    Login Page Edited
    Password text field added, KNUST logo changed to a high-res version and design tweaks made to page.dart

--- librarysec/lib/page.dart
+++ librarysec/lib/page.dart
@@ -16,36 +16,50 @@ class _Page extends State<Pages> with WindowListener {
     return Scaffold(
         body: Center(
             child: Column(
-                //mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
           const Text(
-            'digiTheft',
+            'KNUST LIBRARY PORTAL',
             style: TextStyle(
                 fontSize: 30,
-                color: Colors.purple,
+                color: Color(0xff610600),
                 fontWeight: FontWeight.bold),
           ),
           SizedBox(
             height: 20,
           ),
-          const Image(image: AssetImage('images/download.png')),
+           Image.asset('images/knust_logo_transparent.png', height: 275, width: 275),
           SizedBox(
             height: 20,
           ),
-          Container(
-              height: 20,
-              width: 100,
-              child: TextField(
-                style: const TextStyle(fontSize: 9),
-                decoration: InputDecoration(
-                  labelText: 'Identity',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  errorBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red)),
+          Column(children: <Widget>[
+            SizedBox(width: 400, child: TextField(
+              style: const TextStyle(fontSize: 15),
+              decoration: InputDecoration(
+                labelText: 'Identity',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
                 ),
-              ))
+                errorBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red)),
+              ),
+            )),
+            SizedBox(height: 20),
+            SizedBox(width: 400, child: TextField(
+              style: const TextStyle(fontSize: 15),
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                errorBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red)),
+              ),
+            ))
+
+          ],)
+
         ])));
   }
 }
