ebcc3ecae1cbfdab4cc1d9232658031dcca6cf41
Valdo <85319750+valdodotcom@users.noreply.github.com>
2022-08-23 20:53:22 +0000

    Converted Serial Info From ASCII Values to Characters
     The serial_read_and_write_arduino.dart file has been updated to do so.

--- librarysec/lib/serial_read_and_write/serial_read_and_write_arduino.dart
+++ librarysec/lib/serial_read_and_write/serial_read_and_write_arduino.dart
@@ -1,25 +1,33 @@
-import 'dart:io';
-import 'dart:typed_data';
-
-import 'package:flutter/material.dart';
 import 'package:serial_port_win32/serial_port_win32.dart';
 
 void main() {
   var ports = SerialPort.getAvailablePorts();
   print(ports);
 
+  var converted = [];
+
   if (ports.contains("COM3")) {
     final port = SerialPort("COM3", openNow: false, BaudRate: 9600);
-    print("COM3 port found");
-
     port.open();
-    print("isOpened: ${port.isOpened}");
+    print("COM3 port isOpened?: ${port.isOpened}");
 
-    port.readBytesOnListen(8, (value) => print(value.toString()));
+    port.readBytesOnListen(32, (value) {
+
+      for(final e in value){
+        var currentElement = e;
+          converted.add(String.fromCharCode(int.parse(currentElement.toString())));
+      }
+      print(converted.join());
+    });
+
     String buffer = "True";
     port.writeBytesFromString(buffer);
+    // port.close();
 
   } else {
     print("Unable to find required COM port");
   }
+
 }
+
+
